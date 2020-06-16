dbd076d105b1304408bee7e71bc0e0368ce76558
Jeremy Burman <jburman@gmail.com>
2020-06-16 14:43:43 -0400

    cleanup

--- src/W8lessLabs.BlazorDataTable/wwwroot/js/table.js
+++ src/W8lessLabs.BlazorDataTable/wwwroot/js/table.js
@@ -4,20 +4,20 @@
 window.blazorDataTable = {
     scrollHandlers: {},
     attachEvents: function (rootId) {
-        var theader = document.querySelector('#' + rootId + ' .w8less-header');
-        var tbody = document.querySelector('#' + rootId + ' .w8less-theight');
-        var handler = null;
-        if (theader && tbody) {
-            if (typeof blazorDataTable.scrollHandlers[rootId] === 'function') {
-                console.info('removing function');
-                tbody.removeEventListener('scroll', blazorDataTable.scrollHandlers[rootId]);
+        setTimeout(function () {
+            var theader = document.querySelector('#' + rootId + ' .w8less-header');
+            var tbody = document.querySelector('#' + rootId + ' .w8less-theight');
+            var handler = null;
+            if (theader && tbody) {
+                if (typeof blazorDataTable.scrollHandlers[rootId] === 'function') {
+                    tbody.removeEventListener('scroll', blazorDataTable.scrollHandlers[rootId]);
+                }
+                handler = function (e) {
+                    theader.scrollLeft = tbody.scrollLeft;
+                };
+                tbody.addEventListener('scroll', handler);
+                blazorDataTable.scrollHandlers[rootId] = handler;
             }
-            handler = function (e) {
-                theader.scrollLeft = tbody.scrollLeft;
-            };
-            tbody.addEventListener('scroll', handler);
-            blazorDataTable.scrollHandlers[rootId] = handler;
-        }
-    },
-
+        }, 100);
+    }
 }
